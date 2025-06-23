f7491ef1c74de822b99fe6af64a621382cc22b19
gojo18888 <sayuramaximum@gmail.com>
2025-06-23 13:31:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'Manu-MD&KZExjS6C#2K6DFYPoC3HvJyn9EUUKWWSZs3FsIt7ZGGvtw9K07fI', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
+SESSION_ID: process.env.SESSION_ID || 'Manu-MD&uUFnCDhD#qsKB-0MPDb9tgIYL90V7G-DYvlewpt-9IKer-gt3NSs', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
 PORT: process.env.PORT || '3000',
 MONGODB: process.env.MONGODB || 'mongodb+srv://yesiy77559:edT6bMboc8n0jXDQ@manucustomize.n0rxcdd.mongodb.net/'
 };
